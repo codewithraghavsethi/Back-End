472adceae8e914f016f0f5056de9f6e66b8fdf85
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-30 12:15:27 +0530

    Code

---
+++ Part 1/script.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5]
+
+let newArr = arr.forEach (function(element){
+    console.log(element);
+    
+});
+
+let array = arr.forEach(val=>{
+    console.log(val + " Raghav Sethi");
+});
+
+console.log(array)
+console.log(arr)
+
+// let blog = await fetch(`https://randomuser.me/api/`)
+// let res = await blog.json()
+// console.log(res);
