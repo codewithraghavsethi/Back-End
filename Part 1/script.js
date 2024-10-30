fc12265fcc9aa701b9ba86328a1d280acb287aba
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-10-30 14:29:17 +0530

    code

--- Part 1/script.js
+++ Part 1/script.js
@@ -1,17 +1,20 @@
-let arr = [1,2,3,4,5]
+let arr = [1, 2, 3, 4, 5];
 
-let newArr = arr.forEach (function(element){
-    console.log(element);
-    
+let newArr = arr.forEach(function (element) {
+  console.log(element);
 });
 
-let array = arr.forEach(val=>{
-    console.log(val + " Raghav Sethi");
+let array = arr.forEach((val) => {
+  console.log(val + " Raghav Sethi");
 });
 
-console.log(array)
-console.log(arr)
+console.log(array);
+console.log(arr);
 
-// let blog = await fetch(`https://randomuser.me/api/`)
-// let res = await blog.json()
-// console.log(res);
+async function func() {
+  let blog = await fetch(`https://randomuser.me/api/`);
+  let res = await blog.json();
+  console.log(res.results[0].name);
+};
+
+func();
