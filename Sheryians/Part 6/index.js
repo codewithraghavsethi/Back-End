2fd9ba527b852fd1d4e3eed6c0efc770ba721ff6
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-03 13:57:38 +0530

    Code

--- Sheryians/Part 6/index.js
+++ Sheryians/Part 6/index.js
@@ -3,9 +3,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.send("Chal Raha Hai");
+  res.render("index.ejs");
 });
 
 app.listen(3000, function () {
