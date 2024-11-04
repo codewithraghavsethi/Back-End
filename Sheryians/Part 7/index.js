89d10cd968559defd91854f5d0df3b150fec48ec
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-04 11:08:57 +0530

    Code

--- Sheryians/Part 7/index.js
+++ Sheryians/Part 7/index.js
@@ -9,15 +9,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  fs.readdir("./files", function (err, files) {
+  fs.readdir(`./files`, function (err, files) {
     res.render("index", { files: files });
   });
 });
 
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata) {
+    res.render("show")
+  });
+});
+
 app.post("/create", function (req, res) {
-  fs.writeFile(`./files/${req.body.title.split('').join('')}.txt`, req.body.details, function(err){
-  res.redirect("/")
-  })
+  fs.writeFile(
+    `./files/${req.body.title.split("").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000);
