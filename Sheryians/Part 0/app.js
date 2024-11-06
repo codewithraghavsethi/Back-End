159bf2ccefa7f04c53090fd86f598522c04f7ba5
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-06 17:02:41 +0530

    code

--- Sheryians/Part 0/app.js
+++ Sheryians/Part 0/app.js
@@ -3,8 +3,27 @@ const app = express()
 
 app.set("veiw engine", "ejs")
 
-app.get("/", (req, res)=>{
+app.use((req, res, next)=>{
+    console.log("Hello Ji")
+    next()
+})
+
+app.get("/", (req, res, next)=>{
+
+    let a = 10
+    let b = 3
+    console.log(a+b);
+    next()
+
+}, (req, res)=>{
     res.render("index.ejs")
 })
+
+app.get("/get-form-data", (req, res)=>{
+    console.log(req.query)
+    res.send("Data Recieved")
+
+})
+
 
 app.listen(3000)
