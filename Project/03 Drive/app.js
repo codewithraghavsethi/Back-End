61b33d88d84bd520a37db93d57009c2c90e76565
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-08 10:12:37 +0530

    code

--- Project/03 Drive/app.js
+++ Project/03 Drive/app.js
@@ -1,14 +1,20 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const userRouter = require("./routes/user.routes")
-app.set("view engine", "ejs")
+const userRouter = require("./routes/user.routes");
+const { body, validationResult } = require('express-validator');
 
-app.get("/", (req, res)=>{
-    res.render("index")
-})
 
-app.use("/user", userRouter)
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
-})
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use("/user", userRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- Project/03 Drive/routes/user.routes.js
+++ Project/03 Drive/routes/user.routes.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const { body } = require("express-validator");
+const { validationResult } = require("express-validator");
 const router = express.Router();
 
 // router.get("/test", (req, res) => {
 //   res.send("User test Route");
 // });
 
-router.get("/register", (req, res)=>{
-    res.render("register")
-})
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post(
+  "/register",
+  body("username").trim().isLength({ min: 5 }),
+  body("email").trim().isEmail(),
+  body("password").trim().isLength({ min: 4 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    res.send(errors);
+  }
+);
 
 module.exports = router;
