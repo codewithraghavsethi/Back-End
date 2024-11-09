6ec3da127317ab247856cef7dd5ba5635eb1a070
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-09 09:42:34 +0530

    code

--- Project/03 Drive/routes/user.routes.js
+++ Project/03 Drive/routes/user.routes.js
@@ -3,11 +3,7 @@ const { body } = require("express-validator");
 const { validationResult } = require("express-validator");
 const router = express.Router();
 const userModel = require("../models/user.model");
-const bcrypt = require("bcrypt")
-
-// router.get("/test", (req, res) => {
-//   res.send("User test Route");
-// });
+const bcrypt = require("bcrypt");
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -29,13 +25,47 @@ router.post(
     }
 
     const { email, username, password } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
     const newUser = await userModel.create({
       username,
       email,
-      password,
+      password: hashPassword,
     });
 
     res.json(newUser);
+  }
+);
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post(
+  "/login",
+  body("username").trim().isLength({min: 6}),
+  body("password").trim().isLength({ min: 4 }),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: errors.array(),
+        massage: "Invalid Data",
+      });
+    }
+
+    const { username, password } = req.body;
+    const user = await userModel.findOne({
+        username: username
+    })
+
+    if(!user){
+      return res.status(400).json({
+        massage: "username or password is incorrect"
+      })
+    }
+    
   }
 );
 
