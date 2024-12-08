7e5160b30a16b8e007dce1987e4b5481d9701e1d
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-08 14:09:33 +0530

    code

--- Chai Aur Code/01-V/index.js
+++ Chai Aur Code/01-V/index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/raghav", (req, res)=>{
+  res.send("Hello Raghav")
+})
+
+app.get("/login", (req, res)=>{
+  res.send("Hello Login")
+})
+
+app.get("/signup", (req, res) =>{
+  res.send("Hello Sign-Up")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} - Hello Raghav`)
 })
