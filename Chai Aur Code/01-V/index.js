e7061a9d5a23a80c9ab4e4e2cf553149f9a84c5a
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-09 09:04:10 +0530

    code

--- Chai Aur Code/01-V/index.js
+++ Chai Aur Code/01-V/index.js
@@ -1,6 +1,43 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
-const port = 3000
+// const port = 3000
+
+const githubdata = {
+  "login": "codewithraghavsethi",
+  "id": 131133965,
+  "node_id": "U_kgDOB9DyDQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/131133965?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/codewithraghavsethi",
+  "html_url": "https://github.com/codewithraghavsethi",
+  "followers_url": "https://api.github.com/users/codewithraghavsethi/followers",
+  "following_url": "https://api.github.com/users/codewithraghavsethi/following{/other_user}",
+  "gists_url": "https://api.github.com/users/codewithraghavsethi/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/codewithraghavsethi/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/codewithraghavsethi/subscriptions",
+  "organizations_url": "https://api.github.com/users/codewithraghavsethi/orgs",
+  "repos_url": "https://api.github.com/users/codewithraghavsethi/repos",
+  "events_url": "https://api.github.com/users/codewithraghavsethi/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/codewithraghavsethi/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "RAGHAV SETHI",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "👋 Hello, I'm a passionate full stack developer with a love for building seamless web applications.",
+  "twitter_username": null,
+  "public_repos": 21,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 1,
+  "created_at": "2023-04-18T05:39:03Z",
+  "updated_at": "2024-11-11T17:16:43Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,6 +55,10 @@ app.get("/signup", (req, res) =>{
   res.send("Hello Sign-Up")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port} - Hello Raghav`)
+app.get("/github", (req, res)=> {
+  res.json(githubdata)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT} - Hello Raghav`)
 })
