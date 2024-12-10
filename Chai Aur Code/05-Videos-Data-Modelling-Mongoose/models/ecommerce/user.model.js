24c18e96005a7cbf8e212a15a7a2dac30e9fc092
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-10 11:35:26 +0530

    code

---
+++ Chai Aur Code/05-Videos-Data-Modelling-Mongoose/models/ecommerce/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeseries: true }
+);
+
+export const User = mongoose.model("User", userShema);
