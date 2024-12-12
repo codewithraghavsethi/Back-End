b575de0fdbec5dea5be148ead0c1e98d5ade5dad
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-12 10:57:23 +0530

    code

--- Chai Aur Code/06-Videos-Professional-Backend-Project/backend/src/utils/asyncHandler.js
+++ Chai Aur Code/06-Videos-Professional-Backend-Project/backend/src/utils/asyncHandler.js
@@ -1,3 +1,23 @@
-const asyncHandler = () => {};
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-export { asyncHandler }
+export { asyncHandler };
+
+// const asyncHandler = () => () => {}
+// const asyncHandler = () => {
+//   () => {};
+// };
+
+// const asyncHandler = (fn) => async (res, req, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
